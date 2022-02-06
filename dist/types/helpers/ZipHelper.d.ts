9de56a2ac0add9bb7e00ef69032c23b7c4a5388c
Andrew Molchanov <mr.catmanrus@gmail.com>
2022-02-06 20:22:25 +0300

    Удаляем файлы, переводим обратно на npm

--- dist/types/helpers/ZipHelper.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class ZipHelper {
-    /**
-     * Распаковка архива в папку
-     * @param archive - путь до архива
-     * @param destDir - конечная папка
-     */
-    static unzipArchive(archive: string, destDir: string, whitelist?: string[]): void;
-}
--- dist/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./helpers/HttpHelper";
-export * from "./helpers/JsonHelper";
-export * from "./helpers/ProgressHelper";
-export * from "./helpers/StorageHelper";
-export * from "./helpers/ZipHelper";
