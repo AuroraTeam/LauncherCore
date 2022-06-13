ebc5d8ddc29c2300d06ca46fb0ab1701c4e3c058
Andrew Molchanov <mr.catmanrus@gmail.com>
2022-06-14 02:30:47 +0300

    Marked deprecated fromJSON/toJSON functions

--- src/helpers/JsonHelper.ts
+++ src/helpers/JsonHelper.ts
@@ -7,11 +7,18 @@ export type JsonData = Record<string, any>
 
 export class JsonHelper {
     /**
+     * @deprecated Используйте `JsonHelper.fromJson`
+     */
+    static fromJSON<T>(string: string): T {
+        return this.fromJson(string)
+    }
+
+    /**
      * Преобразовать JSON строку в объект
      * @param string JSON строка
      * @returns `Object | Array`
      */
-    static fromJSON<T>(string: string): T {
+    static fromJson<T>(string: string): T {
         return JSON.parse(string)
     }
 
@@ -20,8 +27,19 @@ export class JsonHelper {
      * @param obj Пробразуемый объект
      * @param pretty Форматировать вывод отступами или вывести в одну строку (по умолчанию `false`)
      * @returns JSON сторка
+     * @deprecated Используйте `JsonHelper.toJson`
      */
     static toJSON(obj: JsonData, pretty = false): string {
+        return this.toJson(obj, pretty)
+    }
+
+    /**
+     * Преобразовать объект в JSON строку
+     * @param obj Пробразуемый объект
+     * @param pretty Форматировать вывод отступами или вывести в одну строку (по умолчанию `false`)
+     * @returns JSON сторка
+     */
+    static toJson(obj: JsonData, pretty = false): string {
         return pretty ? JSON.stringify(obj, null, 4) : JSON.stringify(obj)
     }
 }
