c3f510749bdcec31e3857b5ebcabaec0147bd484
Andrew Molchanov <mr.catmanrus@gmail.com>
2022-09-01 02:00:02 +0300

    added ClientArguments + reexports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-export * from "./helpers/HttpHelper"
-export * from "./helpers/JsonHelper"
-export * from "./helpers/ProgressHelper"
-export * from "./helpers/StorageHelper"
-export * from "./helpers/ZipHelper"
+export * from "./helpers"
+export * from "./interfaces"
